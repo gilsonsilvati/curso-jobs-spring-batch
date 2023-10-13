24a1483cd7506b1ea96ac23ee2afedcce1440d0a
Gilson Silva <gilsonsilvati@gmail.com>
2023-10-13 11:15:14 -0300

    Externalização de propriedades

---
+++ primeiro-projeto-spring-batch/src/main/java/br/com/batch/config/ApplicationPropsConfig.java
@@ -0,0 +1,22 @@
+package br.com.batch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class ApplicationPropsConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer configurer() {
+
+        PropertySourcesPlaceholderConfigurer propertySources = new PropertySourcesPlaceholderConfigurer();
+        propertySources.setLocation(
+                new FileSystemResource("/etc/config/primeiro-projeto-spring-batch/application.properties")
+        );
+
+        return propertySources;
+    }
+
+}
